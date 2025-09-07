0c8a5fab6c78361d40ff2411dbc4ec90f0075eb7
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:43:40 +0000

    Adapt to LMODroid by device-add script

--- device-lineage.mk
+++ device-lineage.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lmodroid
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
